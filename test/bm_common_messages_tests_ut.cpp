744e23615758e62c8148101dae14bd4e90eba4d0
Russell DeGuzman <russell.deguzman@sofarocean.com>
2024-03-18 12:35:22 -0700

    adding device request

--- test/bm_common_messages_tests_ut.cpp
+++ test/bm_common_messages_tests_ut.cpp
@@ -5,10 +5,11 @@
 #include "bm_soft_data_msg.h"
 #include "config_cbor_map_srv_reply_msg.h"
 #include "config_cbor_map_srv_request_msg.h"
+#include "device_test_svc_reply_msg.h"
+#include "device_test_svc_request_msg.h"
 #include "sensor_header_msg.h"
 #include "sys_info_svc_reply_msg.h"
 #include "gtest/gtest.h"
-#include "device_test_svc_reply_msg.h"
 #include <cmath>
 #include <math.h>
 
@@ -134,7 +135,7 @@ TEST_F(BmCommonTest, DeviceTestSvcReplyMsgTestData) {
   DeviceTestSvcReplyMsg::Data d;
   d.success = true;
   d.data_len = 4;
-  d.data = (uint8_t *)malloc(d.data_len);
+  d.data = static_cast<uint8_t *>(malloc(d.data_len));
   d.data[0] = 0x01;
   d.data[1] = 0x02;
   d.data[2] = 0x03;
@@ -142,11 +143,14 @@ TEST_F(BmCommonTest, DeviceTestSvcReplyMsgTestData) {
 
   uint8_t cbor_buffer[1024];
   size_t len = 0;
-  EXPECT_EQ(DeviceTestSvcReplyMsg::encode(d, cbor_buffer, sizeof(cbor_buffer), &len), CborNoError);
+  EXPECT_EQ(
+      DeviceTestSvcReplyMsg::encode(d, cbor_buffer, sizeof(cbor_buffer), &len),
+      CborNoError);
   EXPECT_EQ(len, 30);
 
   DeviceTestSvcReplyMsg::Data decode;
-  EXPECT_EQ(DeviceTestSvcReplyMsg::decode(decode, cbor_buffer, len), CborNoError);
+  EXPECT_EQ(DeviceTestSvcReplyMsg::decode(decode, cbor_buffer, len),
+            CborNoError);
   EXPECT_EQ(decode.success, true);
   EXPECT_EQ(decode.data_len, 4);
   EXPECT_EQ(decode.data[0], 0x01);
@@ -164,12 +168,62 @@ TEST_F(BmCommonTest, DeviceTestSvcReplyMsgTestZeroData) {
 
   uint8_t cbor_buffer[1024];
   size_t len = 0;
-  EXPECT_EQ(DeviceTestSvcReplyMsg::encode(d, cbor_buffer, sizeof(cbor_buffer), &len), CborNoError);
+  EXPECT_EQ(
+      DeviceTestSvcReplyMsg::encode(d, cbor_buffer, sizeof(cbor_buffer), &len),
+      CborNoError);
   EXPECT_EQ(len, 26);
 
   DeviceTestSvcReplyMsg::Data decode;
-  EXPECT_EQ(DeviceTestSvcReplyMsg::decode(decode, cbor_buffer, len), CborNoError);
+  EXPECT_EQ(DeviceTestSvcReplyMsg::decode(decode, cbor_buffer, len),
+            CborNoError);
   EXPECT_EQ(decode.success, true);
+  EXPECT_EQ(decode.data_len, 0);
+  EXPECT_TRUE(decode.data == NULL);
+  free(d.data);
+}
+
+TEST_F(BmCommonTest, DeviceTestSvcRequestMsgTestData) {
+  DeviceTestSvcRequestMsg::Data d;
+  d.data_len = 4;
+  d.data = static_cast<uint8_t *>(malloc(d.data_len));
+  d.data[0] = 0x01;
+  d.data[1] = 0x02;
+  d.data[2] = 0x03;
+  d.data[3] = 0x04;
+
+  uint8_t cbor_buffer[1024];
+  size_t len = 0;
+  EXPECT_EQ(DeviceTestSvcRequestMsg::encode(d, cbor_buffer, sizeof(cbor_buffer),
+                                            &len),
+            CborNoError);
+  EXPECT_EQ(len, 21);
+
+  DeviceTestSvcRequestMsg::Data decode;
+  EXPECT_EQ(DeviceTestSvcRequestMsg::decode(decode, cbor_buffer, len),
+            CborNoError);
+  EXPECT_EQ(decode.data_len, 4);
+  EXPECT_EQ(decode.data[0], 0x01);
+  EXPECT_EQ(decode.data[1], 0x02);
+  EXPECT_EQ(decode.data[2], 0x03);
+  EXPECT_EQ(decode.data[3], 0x04);
+  free(d.data);
+}
+
+TEST_F(BmCommonTest, DeviceTestSvcRequestMsgTestZeroData) {
+  DeviceTestSvcRequestMsg::Data d;
+  d.data_len = 0;
+  d.data = NULL;
+
+  uint8_t cbor_buffer[1024];
+  size_t len = 0;
+  EXPECT_EQ(DeviceTestSvcRequestMsg::encode(d, cbor_buffer, sizeof(cbor_buffer),
+                                            &len),
+            CborNoError);
+  EXPECT_EQ(len, 17);
+
+  DeviceTestSvcRequestMsg::Data decode;
+  EXPECT_EQ(DeviceTestSvcRequestMsg::decode(decode, cbor_buffer, len),
+            CborNoError);
   EXPECT_EQ(decode.data_len, 0);
   EXPECT_TRUE(decode.data == NULL);
   free(d.data);
