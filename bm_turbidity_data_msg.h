f5ed77c475505d13d5a5b13539ae69bfdb5bb5a7
Victor Sowa <victor.sowa@sofarocean.com>
2024-04-22 10:47:51 -0700

    start creating the new files for turbidity

---
+++ bm_turbidity_data_msg.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "cbor.h"
+#include "sensor_header_msg.h"
+
+namespace BmTurbidityDataMsg {
+
+constexpr uint32_t VERSION = 1;
+constexpr size_t NUM_FIELDS = 2 + SensorHeaderMsg::NUM_FIELDS;
+
+struct Data {
+  SensorHeaderMsg::Data header;
+  double s_signal;
+  double r_signal;
+};
+
+CborError encode(Data &d, uint8_t *cbor_buffer, size_t size,
+                 size_t *encoded_len);
+
+CborError decode(Data &d, const uint8_t *cbor_buffer, size_t size);
+
+} // namespace BmTurbidityDataMsg
