60c018d1c64832ec8d08bbab56aa70d6379a10fe
Zachary Crockett <zachary.crockett@sofarocean.com>
2025-05-15 16:23:21 -0700

    feat: add salinity to PME DO message (#50)

--- pme_dissolved_oxygen_msg.h
+++ pme_dissolved_oxygen_msg.h
@@ -7,7 +7,7 @@
 namespace PmeDissolvedOxygenMsg {
 
 constexpr uint32_t VERSION = 1;
-constexpr size_t NUM_FIELDS = 4 + SensorHeaderMsg::NUM_FIELDS;
+constexpr size_t NUM_FIELDS = 5 + SensorHeaderMsg::NUM_FIELDS;
 
 struct Data {
   SensorHeaderMsg::Data header;
@@ -15,6 +15,7 @@ struct Data {
   double do_mg_per_l;
   double quality;
   double do_saturation_pct;
+  float salinity_ppt;
 };
 
 CborError encode(Data &d, uint8_t *cbor_buffer, size_t size,
